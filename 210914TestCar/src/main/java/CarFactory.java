7cab0385f77b86bf636c7b5ed6862cc81b2d4e2d
Lunnen <lunnen@live.se>
2021-09-14 13:57:37 +0200

    Test - Refactoring Andreas, Jonatan, Christer, Susanne

--- 210914TestCar/src/main/java/CarFactory.java
+++ 210914TestCar/src/main/java/CarFactory.java
@@ -2,78 +2,48 @@
 public class CarFactory {
     private String regNr;
     private String color;
-    private String model;
-    private String carEngine;
     private CarModel carModel;
 
     public CarFactory(String color) {
         this.color = color;
 
-        setRegNr(randomRegistration());
-    }
-
-    public String getCarEngine() {
-        return carEngine;
-    }
-
-    public void setCarEngine(String carEngine) {
-        this.carEngine = carEngine;
-    }
-
-    public String getRegNr() {
-        return regNr;
+        setRegNr(generateRegistrationNumber());
     }
 
     public void setRegNr(String regNr) {
         this.regNr = regNr;
     }
 
-    static String randomRegistration()
-    {
-        // Generate three random uppercase letters
-        int letter1 = 65 + (int)(Math.random() * (90 - 65));
-        int letter2 = 65 + (int)(Math.random() * (90 - 65));
-        int letter3 = 65 + (int)(Math.random() * (90 - 65));
+    public String generateRegistrationNumber(){
+        return ("" + (char)(getLetter()) + ((char)(getLetter())) +
+                ((char)(getLetter())) + "" + getNumber() + getNumber() + getNumber());
+    }
 
-        // Generate four random digits
-        int number1 = (int)(Math.random() * 10);
-        int number2 = (int)(Math.random() * 10);
-        int number3 = (int)(Math.random() * 10);
+    private int getNumber() {
+        return (int)(Math.random() * 10);
+    }
 
-        // Display number plate
-        return ("" + (char)(letter1) + ((char)(letter2)) +
-                ((char)(letter3)) + number1 + number2 + number3);
+    private int getLetter() {
+        return 65 + (int) (Math.random() * (90 - 65));
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public CarModel getCarModel() {
         return carModel;
     }
 
-    CarModel createCarModel(String name){
+    void createCarModel(String name){
         switch (name.toLowerCase()) {
             case "volvo" -> {
                 carModel = new Volvo();
-                return carModel;
+                return;
             }
             case "saab" -> {
                 carModel = new Saab();
-                return carModel;
+                return;
             }
         }
         throw new RuntimeException("No matching object could be created");
--- 210914TestCar/src/test/java/CarTest.java
+++ 210914TestCar/src/test/java/CarTest.java
@@ -1,33 +1,36 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CarTest {
 
+    CarFactory output;
+
+    @Before
+    public void init() {
+        output = new CarFactory("red");
+    }
+
     @Test
     public void creation_of_new_car_with_color(){
-        CarFactory output = new CarFactory("red");
-
         assertEquals( "red", output.getColor());
     }
     @Test
     public void creation_of_new_car_model(){
-        CarFactory output = new CarFactory("red");
         output.createCarModel("VOLVO");
 
         assertEquals("Volvo XC90" , output.getCarModel().getCarName());
     }
     @Test
     public void creation_of_new_car_model_Saab(){
-        CarFactory output = new CarFactory("blue");
         output.createCarModel("SAAB");
 
         assertEquals("Saab 900" , output.getCarModel().getCarName());
-        assertEquals("blue" , output.getColor());
+        assertEquals("red" , output.getColor());
     }
     @Test
     public void creation_of_new_car_with_engine(){
-        CarFactory output = new CarFactory("red");
         output.createCarModel("VOLVO");
 
         assertEquals("Volvo XC90" , output.getCarModel().getCarName());
