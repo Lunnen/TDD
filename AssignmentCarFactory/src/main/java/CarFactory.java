cbdf234c1514bf547c18a1c7d57e1f745e120886
Lunnen <lunnen@live.se>
2021-09-15 12:20:07 +0200

    Test of another car model creation Andreas, Susanne, Christer, Jonatan

--- AssignmentCarFactory/src/main/java/CarFactory.java
+++ AssignmentCarFactory/src/main/java/CarFactory.java
@@ -1,11 +1,23 @@
 public class CarFactory {
 
-    public Car createCarModel() {
-        return new Volvo();
+    public Car createCarModel(String model) {
+        if(model.equalsIgnoreCase("volvo")){
+            return new Volvo();
+        }
+        if(model.equalsIgnoreCase("saab")){
+            return new Saab();
+        }
+        return null;
     }
 
-    public Car createCarModel(String color) {
-        return new Volvo(color);
+    public Car createCarModel(String model, String color) {
+        if(model.equalsIgnoreCase("volvo")){
+            return new Volvo(color);
+        }
+        if(model.equalsIgnoreCase("saab")){
+            return new Saab(color);
+        }
+        return null;
     }
 
 }
--- AssignmentCarFactory/src/test/java/CarFactoryTest.java
+++ AssignmentCarFactory/src/test/java/CarFactoryTest.java
@@ -9,15 +9,32 @@ public class CarFactoryTest {
     @Test
     public void new_car_from_factory(){
         carFactory = new CarFactory();
-        Car car = carFactory.createCarModel("Red");
+        Car car = carFactory.createCarModel("Volvo", "Red");
 
         assertEquals( "Red", car.getCarColor());
         assertEquals( "Volvo", car.getCarModel());
         assertFalse( (car.getCarColor().length() == 4), "This will fail" );
 
-        Car car2 = carFactory.createCarModel();
+        Car car2 = carFactory.createCarModel("Volvo");
         assertEquals( "Black", car2.getCarColor());
         assertFalse( (car2.getCarColor().length() == 4), "This will fail" );
+    }
+    @Test
+    public void new_car_from_factory_with_another_model(){
+        carFactory = new CarFactory();
+        Car car = carFactory.createCarModel("Saab", "Yellow");
+
+        assertNotNull( car);
+        assertEquals( "Yellow", car.getCarColor());
+        assertEquals( "Saab", car.getCarModel());
+        assertFalse( (car.getCarColor().length() == 5), "This will fail" );
+
+        Car car2 = carFactory.createCarModel("Volkswagen");
+        assertNull( car2);
+
+        Car car3 = carFactory.createCarModel("Saab");
+        assertEquals( "White", car3.getCarColor());
+        assertFalse( (car3.getCarColor().length() == 4), "This will fail" );
     }
 
 }
