cc90691dbf8bb9150d236fcea25cd971cf927672
Lunnen <lunnen@live.se>
2021-09-15 12:01:56 +0200

    Test of new factory model Andreas, Susanne, Christer, Jonatan

--- AssignmentCarFactory/src/test/java/CarFactoryTest.java
+++ AssignmentCarFactory/src/test/java/CarFactoryTest.java
@@ -23,5 +23,17 @@ public class CarFactoryTest {
         carFactory = new CarFactory();
         assertEquals( "Black", carFactory.getColor());
     }
+    @Test
+    public void new_car_from_factory(){
+        carFactory = new CarFactory();
+        Car car = carFactory.createCarModel("Pink");
+
+        assertEquals( "Pink", car.getCarColor());
+        assertFalse( (car.getCarColor().length() == 3), "This will fail" );
+
+        Car car2 = carFactory.createCarModel();
+        assertEquals( "Black", car2.getCarColor());
+        assertFalse( (car2.getCarColor().length() == 4), "This will fail" );
+    }
 
 }
