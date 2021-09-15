74495e9390604d267bfaed5ba942bc0f4331cb72
Lunnen <lunnen@live.se>
2021-09-15 12:09:07 +0200

    Refactoring factory model & upgraded test Andreas, Susanne, Christer, Jonatan

--- AssignmentCarFactory/src/main/java/CarFactory.java
+++ AssignmentCarFactory/src/main/java/CarFactory.java
@@ -1,24 +1,4 @@
 public class CarFactory {
-    private String registrationNumber;
-    private String color;
-
-    public CarFactory() {
-        setRegistrationNumber(randomRegistration());
-        color = "Black";
-
-    }
-    public CarFactory(String color) {
-        setRegistrationNumber(randomRegistration());
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 
     public Car createCarModel() {
         return new Volvo();
@@ -27,28 +7,5 @@ public class CarFactory {
     public Car createCarModel(String color) {
         return new Volvo(color);
     }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String randomRegistration()
-    {
-        return ("" + (char) (getLetter()) + ((char) (getLetter())) +
-                ((char) (getLetter())) + "" + getNumber() + getNumber() + getNumber());
-    }
-
-    public int getNumber() {
-        return (int) (Math.random() * 10);
-    }
-
-    public int getLetter() {
-        return 65 + (int) (Math.random() * (90 - 65));
-    }
-
 
 }
--- AssignmentCarFactory/src/test/java/CarFactoryTest.java
+++ AssignmentCarFactory/src/test/java/CarFactoryTest.java
@@ -7,29 +7,13 @@ public class CarFactoryTest {
     private CarFactory carFactory;
 
     @Test
-    public void new_car_with_registration_number(){
-        carFactory = new CarFactory();
-
-        assertEquals( 6, carFactory.getRegistrationNumber().length());
-        assertFalse( (carFactory.getRegistrationNumber().length() < 6), "This will fail" );
-    }
-    @Test
-    public void new_car_with_parameter_color(){
-        carFactory = new CarFactory("Blue");
-
-        assertEquals( "Blue", carFactory.getColor());
-        assertFalse( (carFactory.getColor().length() == 0), "This will fail" );
-
-        carFactory = new CarFactory();
-        assertEquals( "Black", carFactory.getColor());
-    }
-    @Test
     public void new_car_from_factory(){
         carFactory = new CarFactory();
-        Car car = carFactory.createCarModel("Pink");
+        Car car = carFactory.createCarModel("Red");
 
-        assertEquals( "Pink", car.getCarColor());
-        assertFalse( (car.getCarColor().length() == 3), "This will fail" );
+        assertEquals( "Red", car.getCarColor());
+        assertEquals( "Volvo", car.getCarModel());
+        assertFalse( (car.getCarColor().length() == 4), "This will fail" );
 
         Car car2 = carFactory.createCarModel();
         assertEquals( "Black", car2.getCarColor());
