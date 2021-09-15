001b23eb94bf9ca2d80d84ad685116d165edf724
Lunnen <lunnen@live.se>
2021-09-15 11:51:40 +0200

    Refactoring of registration number Andreas, Susanne, Christer, Jonatan

--- AssignmentCarFactory/src/main/java/CarFactory.java
+++ AssignmentCarFactory/src/main/java/CarFactory.java
@@ -32,21 +32,19 @@ public class CarFactory {
         this.registrationNumber = registrationNumber;
     }
 
-    static String randomRegistration()
+    public String randomRegistration()
     {
-        // Generate three random uppercase letters
-        int letter1 = 65 + (int)(Math.random() * (90 - 65));
-        int letter2 = 65 + (int)(Math.random() * (90 - 65));
-        int letter3 = 65 + (int)(Math.random() * (90 - 65));
-
-        // Generate four random digits
-        int number1 = (int)(Math.random() * 10);
-        int number2 = (int)(Math.random() * 10);
-        int number3 = (int)(Math.random() * 10);
-
-        // Display number plate
-        return ("" + (char)(letter1) + ((char)(letter2)) +
-                ((char)(letter3)) + number1 + number2 + number3);
+        return ("" + (char) (getLetter()) + ((char) (getLetter())) +
+                ((char) (getLetter())) + "" + getNumber() + getNumber() + getNumber());
     }
+
+    public int getNumber() {
+        return (int) (Math.random() * 10);
+    }
+
+    public int getLetter() {
+        return 65 + (int) (Math.random() * (90 - 65));
+    }
+
 
 }
